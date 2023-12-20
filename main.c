30d093b02ba0244a8a4b94c3be5829a2bbc87149
zaskul <lukee2003@gmail.com>
2023-12-20 02:28:08 +0100

    Added hotseat playability

--- main.c
+++ main.c
@@ -2,30 +2,38 @@
 #include <string.h>
 #include <stdbool.h>
 #include <stdlib.h>
+#include <Windows.h>
+#include <ctype.h>
 
 // fills the board with empty tiles
 void fillBoard(int boardSize, int board[boardSize][boardSize]) {
     for (int i = 0; i < boardSize; i++){
-        for (int j = 0; j < boardSize; j++)
-        {
+        for (int j = 0; j < boardSize; j++){
             board[i][j] = 0;
         }
     }
 }
 
-struct KeyValuePair{
+struct KeyValuePair {
     int key;
     char value[4];
 };
 
-
 // displays the current state of the board
-void displayBoard(int boardSize, int board[boardSize-1][boardSize-1], struct KeyValuePair displayableTiles[3]) {
+void displayBoard(int boardSize, int board[boardSize-1][boardSize-1]){
+
+    // assigns correct symbol to the corresponding value
+    struct KeyValuePair displayableTiles[] = {
+            {0, "[ ]"},
+            {1, "[X]"},
+            {2, "[O]"}
+    };
     int rowLabel = 1;
     int counter = 0;
     char columnLabel = 65;
-    for (int i = 0; i < boardSize; i++) {
-        for (int j = 0; j < boardSize; j++) {
+
+    for (int i = 0; i < boardSize; i++){
+        for (int j = 0; j < boardSize; j++){
             // responsible for printing the letters of alphabet as labels for columns
             if (i == boardSize - 1) {
                 if (counter == i) continue;
@@ -38,7 +46,7 @@ void displayBoard(int boardSize, int board[boardSize-1][boardSize-1], struct Key
                 printf(" %d", rowLabel);
                 rowLabel++;
             }
-                // displays the tile state
+                // displays the tiles
             else {
                 printf("%s", displayableTiles[board[i][j]].value);
             }
@@ -48,11 +56,10 @@ void displayBoard(int boardSize, int board[boardSize-1][boardSize-1], struct Key
 }
 
 bool rowCheck(int boardSize, int board[boardSize][boardSize], int initialRow, int turn){
-    // if the max amount of 'X' or 'O' (1's or 2's) value on board is equal to the current value, it is considered a win on one of the sides
     int wantedResult = turn * boardSize;
     int sumOfRow = 0;
     for (int i = 0; i < boardSize; i++){
-        sumOfRow += board[initialRow][i];
+        sumOfRow += (board[initialRow][i] == turn) ? turn : 0;
     }
     return (sumOfRow == wantedResult) ? true : false;
 }
@@ -61,7 +68,7 @@ bool columnCheck(int boardSize, int board[boardSize][boardSize], int initialColu
     int wantedResult = turn * boardSize;
     int sumOfColumn = 0;
     for (int i = 0; i < boardSize; i++){
-        sumOfColumn += board[i][initialColumn];
+        sumOfColumn += (board[i][initialColumn] == turn) ? turn : 0;
     }
     return (sumOfColumn == wantedResult) ? true : false;
 }
@@ -72,52 +79,71 @@ bool diagonalCheck(int boardSize, int board[boardSize][boardSize], int initialRo
     int sumOfBottomToTop = 0;
     int j = boardSize-1;
     // checks both diagonals
-    if ((initialRow == initialColumn) && boardSize % 2 != 0) {
-        for (int i = 0; i < boardSize; i++) {
-            sumOfTopToBottom += board[i][i];
-            sumOfBottomToTop += board[i][j-i];
+    if ((initialRow == initialColumn) && boardSize % 2 != 0){
+        for (int i = 0; i < boardSize; i++){
+            sumOfTopToBottom += (board[i][i] == turn) ? turn : 0 ;
+            sumOfBottomToTop += (board[i][j-i] == turn) ? turn : 0;
         }
         return (sumOfTopToBottom == wantedResult|| sumOfBottomToTop == wantedResult) ? true : false;
     }
     // checks top to bottom diagonal
     else if (initialRow == initialColumn){
-        for (int i = 0; i < boardSize; i++) {
-            sumOfTopToBottom += board[i][i];
+        for (int i = 0; i < boardSize; i++){
+            sumOfTopToBottom += (board[i][i] == turn) ? turn : 0;
         }
         return (sumOfTopToBottom == wantedResult) ? true : false;
     }
     // checks bottom to top diagonal
     else{
-        for (int i = 0; i < boardSize; i++) {
-            sumOfBottomToTop += board[i][j-i];
+        for (int i = 0; i < boardSize; i++){
+            sumOfBottomToTop += (board[i][j-i] == turn) ? turn : 0;
         }
         return (sumOfBottomToTop == wantedResult) ? true : false;
     }
 }
 
-void victoryCheck(int boardSize, int board[boardSize][boardSize], int turn, int initialRow, int initialColumn) {
-    // check if possible diagonal win
+int victoryCheck(int boardSize, int board[boardSize][boardSize], int turn, int initialRow, int initialColumn) {
+    // check if diagonal win is possible
     if ((initialRow == initialColumn) || (initialRow + initialColumn == boardSize-1)){
         if (diagonalCheck(boardSize, board, initialRow, initialColumn, turn)){
-            printf("GAME OVER DIAGONAL\n");
+            return turn;
         }
     }
-    else if (rowCheck(boardSize, board, initialRow, turn) || columnCheck(boardSize, board, initialColumn, turn)){
-        printf("GAME OVER\n");
+    // check the row and column win conditions
+    if (rowCheck(boardSize, board, initialRow, turn) || columnCheck(boardSize, board, initialColumn, turn)){
+            return turn;
+    }
+    return 0;
+}
+
+// check if winning conditions are met
+void victory(int victoryState, bool *gameOnPtr, int boardSize, int board[boardSize][boardSize]){
+    if (victoryState == 1){
+        system("cls");
+        displayBoard(boardSize+1, board);
+        printf("X won");
+        *gameOnPtr = false;
+    }
+    else if(victoryState == 2){
+        system("cls");
+        displayBoard(boardSize+1, board);
+        printf("O won");
+        *gameOnPtr = false;
     }
 }
 
-void playerMove(int boardSize, int board[boardSize][boardSize], int turn){
+void playerMove(int boardSize, int board[boardSize][boardSize], int turn, bool *gameOnPtr, int *turnPtr){
     char move[4];
     int row;
     char column;
-
+    int columnIntValue;
     // read player input
     if (fgets(move, sizeof(move), stdin) != NULL){
         // check if correct the size of input is correct
         if(move[strlen(move) - 1] != '\n'){
-            printf("%c", move[strlen(move) - 1]);
-            printf("Incorrect move, please try again.\n");
+            printf("Incorrect move, please try again.");
+            Sleep(1000);
+            // clear input buffer until newline character
             while (getchar() != '\n');
         }
         else
@@ -126,59 +152,83 @@ void playerMove(int boardSize, int board[boardSize][boardSize], int turn){
             // remove the newline if there is one
             move[strcspn(move, "\n")] = '\0';
             // char, int order
-            if (sscanf(move, "%c%d", &column, &row) == 2)
-            {
-                // converts to ASCII value of the letter and subtracts for correct column index
-                board[row-1][(int)column-65] = turn;
-                victoryCheck(boardSize, board, turn, row-1, (int)column-65);
+            if (sscanf(move, "%c%d", &column, &row) == 2){
+                columnIntValue = (int)toupper(column)-65;
+                // check if out of bounds
+                if (columnIntValue >= boardSize || row-1 >= boardSize){
+                    printf("Tile out of bounds");
+                    Sleep(1000);
+                    return;
+                }
+                if (board[row-1][columnIntValue] == 0){
+                    // converts to ASCII value of the letter and subtracts for correct column index
+                    board[row-1][columnIntValue] = turn;
+                    victory(victoryCheck(boardSize, board, turn, row-1, columnIntValue), gameOnPtr, boardSize, board);
+                    *turnPtr = (turn == 2) ? 1 : 2;
+                }
+                else{
+                    printf("Tile is already occupied or is invalid");
+                    Sleep(1000);
+                }
+
             }
-            else
-            {
+            else{
                 // int, char order
                 char *endPointer;
                 row = strtol(move, &endPointer, 10);
-                if (*endPointer != '\0')
-                {
+
+                if (*endPointer != '\0'){
+                    columnIntValue = (int)toupper(column)-65;
+                    // check if out of bounds
+                    if (columnIntValue >= boardSize || row-1 >= boardSize){
+                        printf("Tile out of bounds");
+                        Sleep(1000);
+                        return;
+                    }
+                    if (board[row-1][columnIntValue] == 0){
                     column = *endPointer;
-                    board[row-1][(int)column-65] = turn;
-                    victoryCheck(boardSize, board, turn, row-1, (int)column-65);
+                    board[row-1][columnIntValue] = turn;
+                    victory(victoryCheck(boardSize, board, turn, row-1, columnIntValue), gameOnPtr, boardSize, board);
+                    *turnPtr = (turn == 2) ? 1 : 2;
+                    }
+                    else{
+                        printf("Tile is already occupied or is invalid");
+                        Sleep(1000);
+                    }
                 }
-                else
-                {
+                else{
                     printf("Incorrect input");
+                    Sleep(1000);
                 }
             }
         }
     }
-    else
-    {
+    else{
         printf("Error of reading input.");
+        Sleep(1000);
     }
 }
 
 
-
 int main() {
-
-    // assigns correct symbol to the corresponding value
-    struct KeyValuePair displayableTiles[] = {
-            {0, "[ ]"},
-            {1, "[X]"},
-            {2, "[O]"}
-    };
-
     int boardSize;
     bool gameOn = true;
+    bool *gameOnPtr = &gameOn;
+    int turn = 1;
+    int *turnPtr = &turn;
+
 //    scanf("%d", &boardSize);
 
-    boardSize = 4; // max 10
+    boardSize = 3; // max 10
     int board[boardSize][boardSize];
 
     fillBoard(boardSize, board);
 
     while(gameOn){
-        displayBoard(boardSize + 1, board, displayableTiles);
-        playerMove(boardSize, board, 1);
+        system("cls");
+        displayBoard(boardSize + 1, board);
+        playerMove(boardSize, board, turn, gameOnPtr, turnPtr);
+
     }
 
     return 0;
